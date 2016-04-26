8fe783f0156d0e82880c4aa29757e35317a3c2d9
igorvsd <igorvsd@gmail.com>
2016-04-26 14:10:30 +0800

    write stacktrace on retry

--- src/main/java/com/legalmonkeys/test/AbstractCucumberTest.java
+++ src/main/java/com/legalmonkeys/test/AbstractCucumberTest.java
@@ -7,26 +7,31 @@ import cucumber.runtime.RuntimeOptions;
 import cucumber.runtime.io.MultiLoader;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
 import java.util.Properties;
 import java.util.ResourceBundle;
 import java.util.regex.Pattern;
 
+@SuppressWarnings("unused")
 public class AbstractCucumberTest {
 
-    public static final String CUCUMBER_STEPS_PACKAGE_PROPERTY = "cucumber.steps.package";
-    protected static final String STEPS_PACKAGE = ResourceBundle.getBundle("cucumber").getString(CUCUMBER_STEPS_PACKAGE_PROPERTY);
-    protected static final String RESOURCES =
+    private static final String CUCUMBER_STEPS_PACKAGE_PROPERTY = "cucumber.steps.package";
+    private static final String STEPS_PACKAGE = ResourceBundle.getBundle("cucumber").getString(CUCUMBER_STEPS_PACKAGE_PROPERTY);
+    private static final String RESOURCES =
             "src" + File.separator + "test" + File.separator + "resources" + File.separator;
+    private static final String BUILD_TEST_RESULTS = "build/test-results/";
     private ClassLoader classLoader = getClass().getClassLoader();
     private MultiLoader multiLoader = new MultiLoader(classLoader);
 
     private static final int RETRY = Integer.parseInt(System.getProperty("retry.tests", "0"));
 
+    @SuppressWarnings("unused")
     public void run() {
         run(0);
     }
 
-    public void run(int retry) {
+    private void run(int retry) {
         // setup
         RuntimeOptions runtimeOptions = new RuntimeOptions(new Properties());
         runtimeOptions.glue.add(STEPS_PACKAGE);
@@ -73,6 +78,7 @@ public class AbstractCucumberTest {
             if (runtime.getErrors().size() == 0 && runtime.getSnippets().size() > 0) {
                 throw new RuntimeException(runtime.getSnippets().get(0));
             }
+            printErrors(runtime, featureName, scenarioName);
             if (retry < RETRY) {
                 run(retry + 1);
             } else {
@@ -80,6 +86,20 @@ public class AbstractCucumberTest {
                     throw new RuntimeException(runtime.getErrors().get(0));
                 }
             }
+        }
+    }
+
+    private void printErrors(Runtime runtime, String featureName, String scenarioName) {
+        //noinspection ResultOfMethodCallIgnored
+        new File(BUILD_TEST_RESULTS).mkdirs();
+        File destFile = new File(BUILD_TEST_RESULTS + "tests-running.log.txt");
+        try (PrintWriter output = new PrintWriter(new FileWriter(destFile, true))) {
+            output.println(String.format("feature: %s , scenario: %s", featureName, scenarioName));
+            for (Throwable throwable : runtime.getErrors()) {
+                output.println(throwable.getMessage());
+                throwable.printStackTrace(output);
+            }
+        } catch (Exception ignored) {
         }
     }
 }
